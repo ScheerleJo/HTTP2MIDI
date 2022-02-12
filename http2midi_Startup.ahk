0c4217efbf028cdd059d14d5e8f9db701802ac5c
Josia <scheerle.josia@gmail.com>
2022-02-12 01:31:59 +0100

    Fixed Bugs with JS and switched to physical MIDI-Port

---
+++ http2midi_Startup.ahk
@@ -0,0 +1,19 @@
+;HEADER START
+
+;This script is written by Josia Scheerle
+;to start a node.js webserver for HTTP2MIDI via a command prompt
+;Last edit: 12.02.22
+#SingleInstance, force
+;HEADER END
+
+sleep 1000
+BlockInput MouseMove
+ToolTip, Starting Node.js Webserver
+Run, cmd.exe
+Sleep, 1000
+SendInput, npm run startup {Enter}
+BlockInput MouseMoveOff
+Sleep, 1000
+WinWait, npm run startup
+Sleep, 1000
+WinMinimize 
--- index.js
+++ index.js
@@ -2,58 +2,40 @@ const PORT = 8010;
 
 const express = require('express');
 const app = express();
-const url = require('url-parse');
+const url_parse = require('url-parse');
 const midi = require('easymidi');
 
-let midiOutput = new midi.Output("Name", true);
+//let midiOutput = new midi.Output('3-4 CUAD-CAPTURE');
 
-// midiOutput.on('message', (deltaTime, message) =>{
-//     console.log(`m: ${message} d:${deltaTime}`);
-// });
-// midiOutput.openVirtualPort("HTTP-Controller");
-
-/*
-midiOutput.send("noteon", {
-    controller: 84
-    value: 127,
-    channel: 1
-}
-})
-setTimeout(() => {
-    midiOutput.send("noteoff", {
-        note: 12 * 5,
-        velocity: 127,
-        channel: 1
-    })
-}, 200);
-
-
-67  --> g
-67  --> g
-56  --> V
-52  --> R
-59  --> Y
-49  --> I
-50  --> P
-55  --> U
-59  --> Y
-49  --> I
-
-
-
-
-
-
-*/
-
+app.get('/kill', (req, res) => {
+    //midiOutput.close();
+    process.exit();
+});
 app.get('/send', function(req, res){
-    // console.log(url(req.url, true).query);
+    let url = url_parse(req.url, true).query.action;
+    console.log(url);
     res.send('Request recieved!');
-    
-    switch (url(req.url, true).query['action']){
-        case 'startRec': console.log('Recording will be started'); break;
-        case 'stopRec': console.log('Recording will be stopped'); break;
-        case 'setMarker': console.log('Marker will be set'); break;
+    switch (url){
+        case 'startRec': 
+            console.log('Recording will be started'); 
+            // sendMidi(85);
+            break;
+        case 'stopRec': 
+            console.log('Recording will be stopped');
+            // sendMidi(86);
+            break;
+        case 'setMarker':
+            console.log('Marker will be set');
+            // sendMidi(87);
+            break;
+        case 'normalize':
+            console.log('Normalizing Effect will be started');
+            // sendMidi(88)
+            break;
+        case 'exportAudio': 
+            console.log('Exporting Process will be started');
+            // sendMidi(89);
+            break;
         default: console.log('Action not detected! Error!'); break;     
     }
 });
@@ -62,5 +44,17 @@ app.listen(PORT, function(){
     console.log(`Server running on Port ${PORT}`);
 });
 
-function sendMidi(){
+function sendMidi(cc){
+    midiOutput.send("cc", {
+        controller: cc,
+        value: 127,
+        channel: 1
+    });
+    setTimeout(() => {
+        midiOutput.send("cc", {
+            controller: cc,
+            velocity: 0,
+            channel: 1
+        })
+    }, 200);
 }
